1a68ff8f2d916c3a392d84ea30c63abed104aa94
akakaze <m2251000@gmail.com>
2019-11-18 14:07:07 +0800

    Revert "scss"
    This reverts commit f87bc6297387724eb9b5ed7f0c7d4135b652e541.

--- src/App.vue
+++ src/App.vue
@@ -1,21 +1,32 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link>
+    </div>
+    <router-view />
   </div>
 </template>
 
-<script>
-import HelloWorld from "./components/HelloWorld.vue";
+<style>
+#app {
+  font-family: "Avenir", Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+}
 
-export default {
-  name: "app",
-  components: {
-    HelloWorld
-  }
-};
-</script>
+#nav {
+  padding: 30px;
+}
 
-<style lang="scss">
-@import "@/scss/base.scss";
+#nav a {
+  font-weight: bold;
+  color: #2c3e50;
+}
+
+#nav a.router-link-exact-active {
+  color: #42b983;
+}
 </style>
