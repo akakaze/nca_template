482976e085f4a7c48dd5fa187b6cf64c5032a950
akakaze <m2251000@gmail.com>
2019-11-29 17:28:32 +0800

    upgrade + localStorge
    使用localStorge處理configure

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,22 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { app } from 'electron';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {}
+	state: {
+		config: JSON.parse(window.localStorage.getItem(`${app.getName()}.config`) || '[]'),
+	},
+	mutations: {
+		getConfig(key) {
+			return this.state.config[key];
+		},
+		setConfig(key, value) {
+			this.state.config[key] = value;
+			window.localStorage.setItem(`${app.getName()}.config`, this.state.config);
+		},
+	},
+	actions: {},
+	modules: {}
 });
