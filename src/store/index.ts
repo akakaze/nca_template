0bd868d85b612550340f3f017586bb58856b14f5
akakaze <m2251000@gmail.com>
2019-12-01 17:21:41 +0800

    colors + ts
    調整色調
    調整Vuex

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,22 +1,25 @@
 import Vue from "vue";
 import Vuex from "vuex";
-import { app } from 'electron';
+import { app } from "electron";
 
 Vue.use(Vuex);
+// console.log(app);
 
 export default new Vuex.Store({
-	state: {
-		config: JSON.parse(window.localStorage.getItem(`${app.getName()}.config`) || '[]'),
-	},
-	mutations: {
-		getConfig(key) {
-			return this.state.config[key];
-		},
-		setConfig(key, value) {
-			this.state.config[key] = value;
-			window.localStorage.setItem(`${app.getName()}.config`, this.state.config);
-		},
-	},
-	actions: {},
-	modules: {}
+  state: {
+    config: JSON.parse(window.localStorage.getItem(`${app.getName()}.config`) || '[]'),
+  },
+  getters: {
+    getConfig(key) {
+      return state.config[key];
+    }
+  },
+  mutations: {
+    setConfig(state, key, value) {
+    	state.config[key] = value;
+    	window.localStorage.setItem(`${app.getName()}.config`, state.config);
+    },
+  },
+  actions: {},
+  modules: {}
 });
