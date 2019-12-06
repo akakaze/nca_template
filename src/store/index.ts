d8216500bc94a8ce6c0d602e2abc44a8f8bbf52b
akakaze <m2251000@gmail.com>
2019-12-06 17:40:56 +0800

    vuex-module-decorators

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,25 +1,31 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import { Module, VuexModule } from "vuex-module-decorators";
 import { app } from "electron";
 
 Vue.use(Vuex);
 // console.log(app);
 
-export default new Vuex.Store({
-  state: {
-    config: JSON.parse(window.localStorage.getItem(`${app.getName()}.config`) || '[]'),
-  },
-  getters: {
-    getConfig(key) {
-      return state.config[key];
-    }
-  },
-  mutations: {
-    setConfig(state, key, value) {
-    	state.config[key] = value;
-    	window.localStorage.setItem(`${app.getName()}.config`, state.config);
-    },
-  },
-  actions: {},
-  modules: {}
-});
+@Module
+export default class State extends VuexModule {
+	config = JSON.parse(window.localStorage.getItem(`${app.getName()}.config`) || '[]')
+}
+
+// export default new Vuex.Store({
+//   state: {
+//     config: JSON.parse(window.localStorage.getItem(`${app.getName()}.config`) || '[]'),
+//   },
+//   getters: {
+//     getConfig(key) {
+//       return state.config[key];
+//     }
+//   },
+//   mutations: {
+//     setConfig(state, key, value) {
+//     	state.config[key] = value;
+//     	window.localStorage.setItem(`${app.getName()}.config`, state.config);
+//     },
+//   },
+//   actions: {},
+//   modules: {}
+// });
